02c88b08f06219b5e3229d71ac95806d90223806
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2020-01-23 20:25:45 +0700

    Initial commit

---
+++ src/main/java/com/fauzan/project/hack/TheHackApplication.java
@@ -0,0 +1,13 @@
+package com.fauzan.project.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHackApplication.class, args);
+	}
+
+}
