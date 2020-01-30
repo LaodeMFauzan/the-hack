9b14aba04947dfa4ea204075aa1437a81c185b84
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2020-01-30 15:06:55 +0700

    use postgre and read db

---
+++ src/main/java/com/fauzan/project/hack/config/PostgreDatasource.java
@@ -0,0 +1,25 @@
+package com.fauzan.project.hack.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Intellij IDEA
+ * User : fauzan
+ * Date : 29/01/20
+ */
+@Configuration
+public class PostgreDatasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
